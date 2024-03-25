29059bb39fb1d8890b7502c2eb2393e012d461ec
Filip Tkáč <filip.tkac@codingbear.com>
2024-03-25 23:34:14 +0800

    custom dataset

---
+++ patient_dataset.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import os
+from PIL import Image
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+
+class PatientDataset(Dataset):
+    def __init__(self, images_path, labels: pd.DataFrame):
+        self.images_path = images_path
+        self.labels = labels
+        self.transform = transforms.ToTensor()
+
+    def __len__(self):
+        return self.labels.shape[0]
+
+    def __getitem__(self, idx):
+        row = self.labels.iloc[idx]
+        pid = row['patient_id']
+        labels = row.drop('patient_id').values
+
+        images = torch.zeros((100, 128, 128), dtype=torch.float64)
+        image_files = os.listdir(f"{self.images_path}/{pid}")
+        image_files = [os.path.splitext(fname)[0] for fname in image_files]
+        image_files = sorted([int(x) for x in image_files])
+        image_files = [f"{f}.png" for f in image_files]
+
+        for idx, fname in enumerate(image_files):
+            image = Image.open(f"{self.images_path}/{pid}/{fname}").convert("L")
+            tensor = self.transform(image)
+            images[idx] = tensor
+
+        return images, labels
