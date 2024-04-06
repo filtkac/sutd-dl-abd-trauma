4d8b9642f51854e46661bb8b7b26bf5429c90a02
Andy W <andy999coc@gmail.com>
2024-04-06 15:45:48 +0800

    implemented and tested 3D CNN

--- models/cnn_3d.py
+++ models/cnn_3d.py
@@ -6,16 +6,18 @@ from torch.autograd import Variable
 # (bs, 128, 128, 100, 1)
 
 
-def conv_3d_block(in_channels, out_channels, kernels=3, padding=1, kernel_size=(2, 2, 2), stride=(2, 2, 2)):
+def conv_3d_block(in_channels, out_channels, kernels=3, padding=1, pool_kernels=2, pool_stride=2):
     return nn.Sequential(
         nn.Conv3d(in_channels, out_channels, kernel_size=kernels, padding=padding),
         nn.BatchNorm3d(out_channels),
         nn.ReLU(),
-        nn.MaxPool3d(kernel_size, stride),
+        nn.MaxPool3d(kernel_size=pool_kernels, stride=pool_stride),
     )
 
 
-def double_conv_3d_block(in_channels, out_channels, kernels=3, padding=1, kernel_size=(2, 2, 2), stride=(2, 2, 2)):
+def double_conv_3d_block(
+    in_channels, out_channels, kernels=3, padding=1, pool_kernels=2, pool_stride=2, pool_padding=0
+):
     return nn.Sequential(
         nn.Conv3d(in_channels, out_channels, kernel_size=kernels, padding=padding),
         nn.BatchNorm3d(out_channels),
@@ -23,7 +25,7 @@ def double_conv_3d_block(in_channels, out_channels, kernels=3, padding=1, kernel
         nn.Conv3d(out_channels, out_channels, kernel_size=kernels, padding=padding),
         nn.BatchNorm3d(out_channels),
         nn.ReLU(),
-        nn.MaxPool3d(kernel_size=kernel_size, stride=stride),
+        nn.MaxPool3d(kernel_size=pool_kernels, stride=pool_stride, padding=pool_padding),
     )
 
 
@@ -33,7 +35,7 @@ class CNN(nn.Module):
     Modified from https://github.com/xmuyzz/3D-CNN-PyTorch/blob/master/models/C3DNet.py
     """
 
-    def __init__(self, in_channels: int, out_channels: int, depth: int = 64):
+    def __init__(self, in_channels, out_channels, depth=64, height=128, width=128):
         """
         Args:
             in_channels (int): The number of inputs
@@ -41,32 +43,27 @@ class CNN(nn.Module):
             depth (int): The number of slices extracted from the CT Scan
         """
         super().__init__()
-        self.conv_block_1 = conv_3d_block(in_channels, 64, 3, 1, (2, 2, 2), (1, 2, 2))
-        self.conv_block_2 = conv_3d_block(64, 128, 3, 1, (2, 2, 2), (2, 2, 2))
-        self.conv_block_3 = double_conv_3d_block(128, 256, 3, 1, (2, 2, 2), (2, 2, 2))
-        self.conv_block_4 = double_conv_3d_block(256, 512, 3, 1, (2, 2, 2), (2, 2, 2))
 
-        self.conv_block_5 = nn.Sequential(
-            nn.Conv3d(512, 512, kernel_size=3, padding=1),
-            nn.BatchNorm3d(512),
-            nn.ReLU(),
-            nn.Conv3d(512, 512, kernel_size=3, padding=1),
-            nn.BatchNorm3d(512),
-            nn.ReLU(),
-            nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(2, 2, 2), padding=(0, 1, 1)),
-        )
+        # (N, C, D, H, W) -> (N, 1, 64, 128, 128)
+        self.conv_block_1 = conv_3d_block(in_channels, 64)  # output: (N, 64, 32, 64, 64)
+        self.conv_block_2 = conv_3d_block(64, 128)  # output: (N, 128, 16, 32, 32)
+        self.conv_block_3 = double_conv_3d_block(128, 256)  # output: (N, 64, 8, 16, 16)
+        self.conv_block_4 = double_conv_3d_block(256, 512)  # output: (N, 64, 4, 8, 8)
+        self.conv_block_5 = double_conv_3d_block(512, 512)  # output: (N, 512, 2, 4, 4)
+
+        fc_inputs = 512 * (depth // 2**5) * (height // 2**5) * (width // 2**5)  # 5 max pooling layers -> 2^5
 
         self.fc1 = nn.Sequential(
-            nn.Linear((512 * ???), 4096),
+            nn.Linear(fc_inputs, 4096),  # output: (N, 4096)
             nn.ReLU(),
             nn.Dropout(0.5),
         )
         self.fc2 = nn.Sequential(
-            nn.Linear(4096, 4096),
+            nn.Linear(4096, 4096),  # output: (N, 4096)
             nn.ReLU(),
             nn.Dropout(0.5),
         )
-        self.fc3 = nn.Sequential(nn.Linear(4096, out_channels)) # TODO: how can we handle 
+        self.fc3 = nn.Linear(4096, out_channels)  # (N, output_channels)
 
     def forward(self, x):
         out = self.conv_block_1(x)
@@ -87,9 +84,12 @@ class CNN(nn.Module):
 
 
 if __name__ == "__main__":
-
-    model = CNN(in_channels=1, out_channels=10)  # 10 classes
+    batch_size, channels, depth, height, width = 32, 1, 64, 128, 128
+    n_classes = 5
+    model = CNN(in_channels=channels, out_channels=n_classes, depth=depth, height=height, width=width)  # 5 classes
     print(model)
 
-    input_var = Variable(torch.randn(32, 1, 100, 128, 128))
+    input_var = Variable(torch.randn(batch_size, channels, depth, height, width))
     output = model(input_var)
+    assert output.shape == (batch_size, n_classes)
+    print("Tested 3D CNN Model")
