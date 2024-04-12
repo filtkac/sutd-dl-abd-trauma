0feebc42af02c2542f56a7902df38fc8ba3869b0
Andy W <andy999coc@gmail.com>
2024-04-12 18:30:13 +0800

    added losses, dataset variable CT slicing, and updated labels nb to show imbalances

--- train.py
+++ train.py
@@ -74,7 +74,6 @@ def train(args):
             # zero gradients for every batch
             optimizer.zero_grad()
             outputs = model(inputs.float())
-            print(outputs)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
---
+++ vastai/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install additional dependencies
+pip install huggingface_hub
+pip install accelerate
+pip install wandb
+pip install tqdm
+pip install pandas matplotlib
